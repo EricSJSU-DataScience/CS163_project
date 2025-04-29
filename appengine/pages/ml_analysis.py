28ce84ace8a0843a6277f02f9fe48f9548f91d3c
Eric <158229984+EricSJSU-DataScience@users.noreply.github.com>
2025-04-29 15:16:29 -0700

    Update ml_analysis.py

--- appengine/pages/ml_analysis.py
+++ appengine/pages/ml_analysis.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html
 from pages.ml_analysis_lstm import lstm_component
+from pages.ml_analysis_rsf import rsf_component
 
 dash.register_page(__name__, path="/ml", name="Analysis-ML Analysis", order=5)
 
@@ -8,9 +9,9 @@ dash.register_page(__name__, path="/ml", name="Analysis-ML Analysis", order=5)
 layout = html.Div(className="container mt-4", children=[
     html.H1("Machine Learning Analysis"),
 
+    # # LSTM
     lstm_component,
 
-    # # LSTM
     # html.H2("Long Short Term Memory (LSTM)"),
     # html.H4("LSTM – Model Architecture"),
     # html.Img(src="/assets/ml1.png", className="img-fluid", style={"max-width": "800px"}),
@@ -26,16 +27,18 @@ layout = html.Div(className="container mt-4", children=[
 
 
     # # RSF
-    html.H2("Random Survival Forest (RSF)"),
-    html.H4("MLA – Model Parameters"),
-    html.Img(src="/assets/mla.png", className="img-fluid", style={"max-width": "800px"}),
-    html.P("The RSF model is configured with 6 trees, depth of 5, and uses 'Council District' and 'NAICS Code' to estimate survival probability of businesses over time."),
+    rsf_component,
 
-    html.H4("MLB – Predicted Survival Curve"),
-    html.Img(src="/assets/mlb.png", className="img-fluid", style={"max-width": "800px"}),
-    html.P("For a business starting in 2026 in Council District 3 and NAICS Code 53, the survival probability declines from 50% to below 20% within 60 months."),
+    # html.H2("Random Survival Forest (RSF)"),
+    # html.H4("MLA – Model Parameters"),
+    # html.Img(src="/assets/mla.png", className="img-fluid", style={"max-width": "800px"}),
+    # html.P("The RSF model is configured with 6 trees, depth of 5, and uses 'Council District' and 'NAICS Code' to estimate survival probability of businesses over time."),
 
-    html.H4("MLC – Model Performance (C-index)"),
-    html.Img(src="/assets/mlc.png", className="img-fluid", style={"max-width": "800px"}),
-    html.P("The model achieves a Concordance Index of 0.7574, indicating strong predictive power and consistent ranking of business survival times.")
+    # html.H4("MLB – Predicted Survival Curve"),
+    # html.Img(src="/assets/mlb.png", className="img-fluid", style={"max-width": "800px"}),
+    # html.P("For a business starting in 2026 in Council District 3 and NAICS Code 53, the survival probability declines from 50% to below 20% within 60 months."),
+
+    # html.H4("MLC – Model Performance (C-index)"),
+    # html.Img(src="/assets/mlc.png", className="img-fluid", style={"max-width": "800px"}),
+    # html.P("The model achieves a Concordance Index of 0.7574, indicating strong predictive power and consistent ranking of business survival times.")
 ])
